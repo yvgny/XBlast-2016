be150ae9083054d6a908c36fc91cd7fe31163817
Sacha Kozma <sacha.kozma@gmail.com>
2016-02-28 23:17:10 +0100

    Coded until ofRows and ofInnerBlocksWalled methods of class Board

--- src/ch/epfl/xblast/Tests.java
+++ src/ch/epfl/xblast/Tests.java
@@ -1,12 +1,15 @@
 package ch.epfl.xblast;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import ch.epfl.cs108.Sq;
+import ch.epfl.xblast.server.Block;
+import ch.epfl.xblast.server.Board;
 
 public class Tests {
 
     public static void main(String[] args) {
-        ArrayList<String> test = new ArrayList<String>();
+        /*ArrayList<String> test = new ArrayList<String>();
         test.add("k");
         //test.add("a");
         //test.add("y");
@@ -21,7 +24,21 @@ public class Tests {
         
         for (String string : test) {
             System.out.println(string);
-        }
+        } */
+        
+        ArrayList<Sq<String>> BoardList = new ArrayList<Sq<String>>();
+        
+        Sq<String> s = Sq.constant("bla");
+        
+        BoardList.add(s);
+        
+        ArrayList<Sq<String>> BoardListCopy = new ArrayList<Sq<String>>(BoardList);
+        
+        BoardListCopy.get(0);
+
+        
+        // Board test = new Board(BoardList);
+        
         
         
     }
--- src/ch/epfl/xblast/server/Board.java
+++ src/ch/epfl/xblast/server/Board.java
@@ -1,8 +1,11 @@
 package ch.epfl.xblast.server;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import ch.epfl.cs108.Sq;
+import ch.epfl.xblast.Cell;
 
 /**
  * 
@@ -11,11 +14,85 @@ import ch.epfl.cs108.Sq;
  *
  */
 public final class Board {
-    
-    private List<Sq<Block>> BoardList;
 
-    public Board(List<Sq<Block>> blocks) {
-        // constrcuteur de copie !
+    private List<Sq<Block>> boardList;
+
+    public Board(List<Sq<Block>> blocks) throws IllegalArgumentException {
+        if (blocks.size() != Cell.COUNT) {
+            throw new IllegalArgumentException(
+                    "La liste ne contient pas " + Cell.COUNT + " éléments.");
+        }
+
+        boardList = new ArrayList<Sq<Block>>(blocks);
+    }
+
+    public static Board ofRows(List<List<Block>> rows)
+            throws IllegalArgumentException {
+        if (rows.size() != Cell.ROWS) {
+            throw new IllegalArgumentException(
+                    "Le pleateau doit contenir " + Cell.ROWS + " lignes.");
+        }
+
+        List<Sq<Block>> sqBlockList = new ArrayList<Sq<Block>>();
+
+        for (List<Block> list : rows) {
+            if (list.size() != Cell.COLUMNS) {
+                throw new IllegalArgumentException("Le plateau doit contenir "
+                        + Cell.COLUMNS + " colonnes.");
+            } else {
+                for (Block block : list) {
+                    sqBlockList.add(Sq.constant(block));
+                }
+            }
+        }
+
+        return new Board(sqBlockList);
+    }
+
+    public static Board ofInnerBlocksWalled(List<List<Block>> innerBlocks)
+            throws IllegalArgumentException {
+        if (innerBlocks.size() != Cell.ROWS - 2) {
+            throw new IllegalArgumentException(
+                    "La liste doit contenir " + (Cell.ROWS - 2) + " lignes.");
+        }
+
+        List<Sq<Block>> sqBlockList = new ArrayList<Sq<Block>>();
+
+        for (List<Block> list : innerBlocks) {
+            if (list.size() != Cell.COLUMNS - 2) {
+                throw new IllegalArgumentException("La liste doit contenir "
+                        + (Cell.COLUMNS - 2) + " colonnes.");
+
+            } else {
+                sqBlockList.add(Sq.constant(Block.INDESTRUCTIBLE_WALL));
+
+                for (Block block : list) {
+                    sqBlockList.add(Sq.constant(block));
+                }
+
+                sqBlockList.add(Sq.constant(Block.INDESTRUCTIBLE_WALL));
+
+            }
+
+            sqBlockList.addAll(Collections.nCopies(Cell.COLUMNS,
+                    Sq.constant(Block.INDESTRUCTIBLE_WALL))); // ajoute le mur
+                                                              // sud
+
+            sqBlockList.addAll(0, Collections.nCopies(Cell.COLUMNS,
+                    Sq.constant(Block.INDESTRUCTIBLE_WALL))); // ajoute le mur
+                                                              // nord
+
+        }
+
+        return new Board(sqBlockList);
+    }
+
+    public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quadrantNWBlocks)
+                    throws IllegalArgumentException {
+        
+        //TODO
+
+        return null;
     }
 
 }
