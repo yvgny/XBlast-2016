3e991926ca90bc4dac592d08bde63b559a5d7120
Sacha Kozma <sacha.kozma@gmail.com>
2016-03-02 13:04:01 +0100

    Add test for Lists

--- src/ch/epfl/xblast/Tests.java
+++ src/ch/epfl/xblast/Tests.java
@@ -1,6 +1,7 @@
 package ch.epfl.xblast;
 
 import java.util.Arrays;
+import java.util.List;
 
 import ch.epfl.xblast.server.Block;
 import ch.epfl.xblast.server.Board;
@@ -11,19 +12,22 @@ public class Tests {
         Block __ = Block.FREE;
         Block II = Block.INDESTRUCTIBLE_WALL;
         Block xx = Block.DESTRUCTIBLE_WALL;
-        Board board = Board.ofInnerBlocksWalled(
-          Arrays.asList(
-            Arrays.asList(__, __, __, __, __, xx, __, __, __, __, __, xx, __),
-            Arrays.asList(__, II, xx, II, xx, II, xx, __, __, __, __, xx, __),
-            Arrays.asList(__, xx, __, __, __, xx, __, __, __, __, __, __, __),
-            Arrays.asList(xx, II, __, II, II, II, II, __, __, __, __, __, __),
-            Arrays.asList(__, xx, __, xx, __, __, __, __, __, __, __, xx, __),
-            Arrays.asList(__, __, __, __, __, xx, __, __, __, __, __, xx, II),
-            Arrays.asList(__, II, xx, II, xx, II, xx, __, __, __, __, xx, __),
-            Arrays.asList(__, xx, __, __, __, xx, __, __, __, __, __, __, __),
-            Arrays.asList(xx, II, __, II, II, II, II, __, __, __, __, __, __),
-            Arrays.asList(__, xx, __, xx, __, __, __, __, __, __, __, xx, __),
-            Arrays.asList(xx, II, xx, II, xx, II, __, __, __, __, __, __, xx)));
+        List<List<Block>> array;
+        
+        array = Arrays.asList(
+                Arrays.asList(__, __, __, __, __, xx, __, __, __, __, __, xx, __),
+                Arrays.asList(__, II, xx, II, xx, II, xx, __, __, __, __, xx, __),
+                Arrays.asList(__, xx, __, __, __, xx, __, __, __, __, __, __, __),
+                Arrays.asList(xx, II, __, II, II, II, II, __, __, __, __, __, __),
+                Arrays.asList(__, xx, __, xx, __, __, __, __, __, __, __, xx, __),
+                Arrays.asList(__, __, __, __, __, xx, __, __, __, __, __, xx, II),
+                Arrays.asList(__, II, xx, II, xx, II, xx, __, __, __, __, xx, __),
+                Arrays.asList(__, xx, __, __, __, xx, __, __, __, __, __, __, __),
+                Arrays.asList(xx, II, __, II, II, II, II, __, __, __, __, __, __),
+                Arrays.asList(__, xx, __, xx, __, __, __, __, __, __, __, xx, __),
+                Arrays.asList(xx, II, xx, II, xx, II, __, __, __, __, __, __, xx));
+        
+        Board board = Board.ofInnerBlocksWalled(array);
         
         Board board2 = Board.ofQuadrantNWBlocksWalled(
                 Arrays.asList(
@@ -49,9 +53,28 @@ public class Tests {
                 System.out.print(", ");
                 
             }
-            
             System.out.println();
         }
+        
+        System.out.println("Tableau array : ");
+        
+        for (int i = 0; i < array.size(); i++) {
+            for (int j = 0; j < array.get(i).size(); j++) {
+                if (array.get(i).get(j) == Block.FREE) {
+                    System.out.print("__");
+                } else if (array.get(i).get(j) == Block.INDESTRUCTIBLE_WALL) {
+                    System.out.print("II");
+                } else if (array.get(i).get(j) == Block.DESTRUCTIBLE_WALL) {
+                    System.out.print("xx");
+                } else {
+                    System.out.print("??");
+                }
+                System.out.print(", ");
+                
+            }
+            System.out.println();
+        }
+        
         
     }
     
---
+++ tests/ch/epfl/xblast/ListsTest.java
@@ -0,0 +1,36 @@
+package ch.epfl.xblast;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListsTest {
+
+    @Test
+    public void mirroredTest() {
+        // one element
+        ArrayList<Character> myList = new ArrayList<>();
+        myList.add('r');
+        List<Character> testList = Lists.mirrored(myList);
+        List<Character> result = Arrays.asList('r');
+        assertEquals(result, testList);
+        
+        // two elements
+        myList.add('a');
+        testList = Lists.mirrored(myList);
+        result = Arrays.asList('r', 'a', 'r');
+        assertEquals(result, testList);
+        
+        // more elements
+        myList.add('c');
+        myList.add('e');
+        testList = Lists.mirrored(myList);
+        result = Arrays.asList('r', 'a', 'c', 'e', 'c', 'a', 'r');
+        assertEquals(result, testList);
+    }
+
+}
