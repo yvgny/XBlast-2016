ff7f89ffc7866999a2493afc6a05ddd5d1653227
Sacha Kozma <sacha.kozma@gmail.com>
2016-03-03 14:23:59 +0100

    Javadoc added

--- src/ch/epfl/xblast/server/Block.java
+++ src/ch/epfl/xblast/server/Block.java
@@ -7,8 +7,31 @@ package ch.epfl.xblast.server;
  *
  */
 public enum Block {
-    FREE, INDESTRUCTIBLE_WALL, DESTRUCTIBLE_WALL, CRUMBLING_WALL;
+    /**
+     * Représente une case libre
+     */
+    FREE,
 
+    /**
+     * Représente une case avec un mur indestructible
+     */
+    INDESTRUCTIBLE_WALL,
+
+    /**
+     * Représente une case avec un mur destructible
+     */
+    DESTRUCTIBLE_WALL,
+
+    /**
+     * Représente une case avec un mur en train de se casser
+     */
+    CRUMBLING_WALL;
+
+    /**
+     * Vérifie si la case est libre
+     * 
+     * @return true si elle est libre (FREE), false sinon
+     */
     public boolean isFree() {
         if (this == Block.FREE) {
             return true;
@@ -19,15 +42,22 @@ public enum Block {
 
     }
 
+    /**
+     * Vérifie si le joueur peut venir sur cette case, donc si la case est libre
+     * (FREE)
+     * 
+     * @return true si la case est libre, false sinon
+     */
     public boolean canHostPlayer() {
-        if (this == Block.FREE) {
-            return true;
-
-        } else {
-            return false;
-        }
+        return isFree();
     }
 
+    /**
+     * Vérifie si la case projette une ombre sur le tableau
+     * 
+     * @return vrai si la case projette une ombre (si c'est une case de type
+     *         mur), flase sinon
+     */
     public boolean castsShadow() {
         if (this.toString().endsWith("_WALL")) {
             return true;
--- src/ch/epfl/xblast/server/Board.java
+++ src/ch/epfl/xblast/server/Board.java
@@ -17,7 +17,19 @@ import ch.epfl.xblast.Lists;
 public final class Board {
 
     private List<Sq<Block>> boardList;
+    private final static int quadrantNWBlocksRows = ((Cell.ROWS - 2) / 2) + 1;
+    private final static int quadrantNWBlocksColumns = ((Cell.COLUMNS - 2) / 2) + 1;
 
+    /**
+     * Construit un tableau a partir d'une liste de séquence de blocs
+     * 
+     * @param blocks
+     *            La liste de séquence de bloc qui représente le tableau dans
+     *            l'ordre de lecture
+     * @throws IllegalArgumentException
+     *             Si la liste ne contient pas
+     *             {@value ch.epfl.xblast.Cell#COUNT} éléments
+     */
     public Board(List<Sq<Block>> blocks) throws IllegalArgumentException {
 
         if (blocks.size() != Cell.COUNT) {
@@ -27,6 +39,17 @@ public final class Board {
         boardList = new ArrayList<Sq<Block>>(blocks);
     }
 
+    /**
+     * Construit un plateau constant avec la matrice de blocs donnée
+     * 
+     * @param rows
+     *            La matrice de blocs utilisée pour construire le plateau
+     * @return La plateau construit a l'aide de la matrice passée en paramètre
+     * @throws IllegalArgumentException
+     *             Si la liste reçue n'est pas constituée de
+     *             {@value ch.epfl.xblast.Cell#ROWS} listes de
+     *             {@value ch.epfl.xblast.Cell#COLUMNS} éléments chacune
+     */
     public static Board ofRows(List<List<Block>> rows) throws IllegalArgumentException {
 
         checkBlockMatrix(rows, Cell.ROWS, Cell.COLUMNS);
@@ -42,6 +65,18 @@ public final class Board {
         return new Board(sqBlockList);
     }
 
+    /**
+     * Construit un plateau muré avec les blocs intérieurs donnés
+     * 
+     * @param innerBlocks
+     *            Les blocs intérieurs a utilisé pour construire le plateau
+     * @return La pleateua construit a l'aide des blocs intérieurs passé en
+     *         paramètre
+     * @throws IllegalArgumentException
+     *             si la liste reçue n'est pas constituée de (
+     *             {@value ch.epfl.xblast.Cell#ROWS} - 2) listes de (
+     *             {@value ch.epfl.xblast.Cell#COLUMNS} - 2) éléments chacune
+     */
     public static Board ofInnerBlocksWalled(List<List<Block>> innerBlocks) throws IllegalArgumentException {
 
         checkBlockMatrix(innerBlocks, Cell.ROWS - 2, Cell.COLUMNS - 2);
@@ -61,9 +96,22 @@ public final class Board {
         return ofRows(copiedInnerBlocks);
     }
 
+    /**
+     * Construit un plateau muré symétrique avec les blocs du quadrant
+     * nord-ouest donnés
+     * 
+     * @param quadrantNWBlocks
+     *            Les blocs du qaudrant nord-ouest à utiliser pour construire le
+     *            plateau
+     * @return Le plateau construit à l'aide du cadran nord-ouest passé en
+     *         paramètre
+     * @throws IllegalArgumentException
+     *             Si la liste reçue n'est pas constituée de 6 listes de 7
+     *             éléments chacune
+     */
     public static Board ofQuadrantNWBlocksWalled(List<List<Block>> quadrantNWBlocks) throws IllegalArgumentException {
 
-        checkBlockMatrix(quadrantNWBlocks, ((Cell.ROWS - 2) / 2) + 1, ((Cell.COLUMNS - 2) / 2) + 1);
+        checkBlockMatrix(quadrantNWBlocks, quadrantNWBlocksRows, quadrantNWBlocksColumns);
 
         List<List<Block>> quadrantNWBlocksWalled = new ArrayList<List<Block>>();
 
@@ -74,6 +122,19 @@ public final class Board {
         return ofInnerBlocksWalled(Lists.mirrored(quadrantNWBlocksWalled));
     }
 
+    /**
+     * Vérifie si la matrice est conforme
+     * 
+     * @param matrix
+     *            La matrice à vérifier
+     * @param rows
+     *            La nombre de ligne que doit posséder la matrice
+     * @param columns
+     *            Le nombre de colonnes que doit posséder la matrice
+     * @throws IllegalArgumentException
+     *             Si la matrice est nulle ou ne contient pas le nombre de
+     *             lignes et de colonnes passé en paramètre
+     */
     public static void checkBlockMatrix(List<List<Block>> matrix, int rows, int columns) throws IllegalArgumentException {
         if (matrix == null) {
             throw new IllegalArgumentException("L'objet est vide");
@@ -89,10 +150,24 @@ public final class Board {
 
     }
 
+    /**
+     * Retourne la séquence des blocs pour la case données
+     * 
+     * @param c
+     *            La case pour laquelle la séquence doit être trouvée
+     * @return La séquence des blocs demandée
+     */
     public Sq<Block> blocksAt(Cell c) {
         return boardList.get((c.y() * Cell.COLUMNS) + c.x());
     }
 
+    /**
+     * Retourne le bloc pour la case donnée
+     * 
+     * @param c
+     *            La case pour laquelle le bloc doit être trouvé
+     * @return Le bloc de la case passée en paramètre
+     */
     public Block blockAt(Cell c) {
         return blocksAt(c).head();
     }
