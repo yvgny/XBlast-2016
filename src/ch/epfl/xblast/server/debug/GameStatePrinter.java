f5ac5b28f24905e7b832c12d772ea0851f7237a4
Sacha Kozma <sacha.kozma@gmail.com>
2016-03-28 18:57:15 +0200

    BlastedCells information added

--- src/ch/epfl/xblast/server/debug/GameStatePrinter.java
+++ src/ch/epfl/xblast/server/debug/GameStatePrinter.java
@@ -1,6 +1,7 @@
 package ch.epfl.xblast.server.debug;
 
 import java.util.List;
+import java.util.Set;
 
 import ch.epfl.xblast.Cell;
 import ch.epfl.xblast.server.Block;
@@ -9,11 +10,15 @@ import ch.epfl.xblast.server.GameState;
 import ch.epfl.xblast.server.Player;
 
 public final class GameStatePrinter {
-    private GameStatePrinter() {}
+    private GameStatePrinter() {
+    }
 
     public static void printGameState(GameState s) {
         List<Player> ps = s.alivePlayers();
+        Set<Cell> blastedCell = s.blastedCells();
         Board board = s.board();
+
+        // Ajout des coordonnées pour x
         System.out.print("  ");
         for (int i = 0; i < Cell.COLUMNS; i++) {
             if (i < 10) {
@@ -23,23 +28,30 @@ public final class GameStatePrinter {
             }
         }
         System.out.println();
-        
+
         for (int y = 0; y < Cell.ROWS; ++y) {
+            // Ajout des coordonnées pour y
             if (y < 10) {
-                System.out.print(y+ " ");
+                System.out.print(y + " ");
             } else {
                 System.out.print(y);
             }
+
             xLoop: for (int x = 0; x < Cell.COLUMNS; ++x) {
                 Cell c = new Cell(x, y);
-                for (Player p: ps) {
+                for (Player p : ps) {
                     if (p.position().containingCell().equals(c)) {
                         System.out.print(stringForPlayer(p));
                         continue xLoop;
                     }
                 }
-                Block b = board.blockAt(c);
-                System.out.print(stringForBlock(b));
+                
+                if (blastedCell.contains(c)) {
+                    System.out.print("XX");
+                } else {
+                    Block b = board.blockAt(c);
+                    System.out.print(stringForBlock(b));
+                }
             }
             System.out.println();
         }
@@ -49,23 +61,38 @@ public final class GameStatePrinter {
         StringBuilder b = new StringBuilder();
         b.append(p.id().ordinal() + 1);
         switch (p.direction()) {
-        case N: b.append('^'); break;
-        case E: b.append('>'); break;
-        case S: b.append('v'); break;
-        case W: b.append('<'); break;
+        case N:
+            b.append('^');
+            break;
+        case E:
+            b.append('>');
+            break;
+        case S:
+            b.append('v');
+            break;
+        case W:
+            b.append('<');
+            break;
         }
         return b.toString();
     }
 
     private static String stringForBlock(Block b) {
         switch (b) {
-        case FREE: return "  ";
-        case INDESTRUCTIBLE_WALL: return "##";
-        case DESTRUCTIBLE_WALL: return "??";
-        case CRUMBLING_WALL: return "¿¿";
-        case BONUS_BOMB: return "+b";
-        case BONUS_RANGE: return "+r";
-        default: throw new Error();
+        case FREE:
+            return "  ";
+        case INDESTRUCTIBLE_WALL:
+            return "##";
+        case DESTRUCTIBLE_WALL:
+            return "??";
+        case CRUMBLING_WALL:
+            return "¿¿";
+        case BONUS_BOMB:
+            return "+b";
+        case BONUS_RANGE:
+            return "+r";
+        default:
+            throw new Error();
         }
     }
 }
