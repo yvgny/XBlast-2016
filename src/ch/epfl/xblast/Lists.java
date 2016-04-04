79b2e50dc0b5e6eca4e133b174d293c7d8e07f9a
Sacha Kozma <sacha.kozma@gmail.com>
2016-04-04 21:39:13 +0200

    Deleted public methods

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -76,42 +76,8 @@ public final class Lists {
                 }
             }
             
-            return permutations;
+             return permutations;
         }
     }
 
-    /**
-     * Copie la liste de liste passée en paramètre. Il faut toutefois faire
-     * attention que seul la référence l'objet de type T est copié, il se s'agit
-     * donc pas de copie profonde
-     * 
-     * @param l
-     *            La liste de liste à copier
-     * @param <T>
-     *            Le type de la liste passée en paramètre
-     * @return Une nouvelle liste de liste de même contenu que celle passée en
-     *         paramètre
-     * @throws IllegalArgumentException
-     *             Si la liste est vide ou si l'objet est nul
-     */
-    public static <T> List<List<T>> copy(List<List<T>> l) throws IllegalArgumentException {
-        if (l == null || l.isEmpty()) {
-            throw new IllegalArgumentException("List is empty or not inizializated !");
-        }
-
-        List<List<T>> copied = new ArrayList<List<T>>();
-
-        for (List<T> list : l) {
-            List<T> temporary = new ArrayList<T>();
-
-            for (T t : list) {
-                temporary.add(t);
-
-            }
-
-            copied.add(temporary);
-        }
-
-        return copied;
-    }
 }
