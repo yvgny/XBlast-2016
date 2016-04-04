0deaa5c8be8853500ddb2b4a1420211d469b72a7
Sacha Kozma <sacha.kozma@gmail.com>
2016-04-04 21:39:02 +0200

    Added detailed debug informations

--- tests/ch/epfl/xblast/RandomGameSimulator.java
+++ tests/ch/epfl/xblast/RandomGameSimulator.java
@@ -20,25 +20,12 @@ import ch.epfl.xblast.server.debug.RandomEventGenerator;
 public class RandomGameSimulator {
 
     public static void main(String[] args) throws InterruptedException {
-        // System.out.println(Block.FREE.isFree());
-        // System.out.println(Block.DESTRUCTIBLE_WALL.isFree());
+        RandomEventGenerator randomEvent = new RandomEventGenerator(2016, 30, 100);
 
-        /*
-         * List<Sq<Block>> m = new ArrayList<>();
-         * m.add(Sq.constant(Block.DESTRUCTIBLE_WALL)); for (int i = 0; i < 194;
-         * ++i) { m.add(Sq.constant(Block.FREE)); } Board b1 = new Board(m);
-         * 
-         * Cell c = new Cell(0, 0); System.out.println(b1.blockAt(c));
-         */
-
-        // ------------------------------------------------------------------------------
-
-        RandomEventGenerator randomEvent = new RandomEventGenerator(2016, 330, 100);
-        
         Block __ = Block.FREE;
         Block XX = Block.INDESTRUCTIBLE_WALL;
         Block xx = Block.DESTRUCTIBLE_WALL;
-        Board board2 = Board.ofQuadrantNWBlocksWalled(
+        Board board1 = Board.ofQuadrantNWBlocksWalled(
                 Arrays.asList(Arrays.asList(__, __, __, __, __, xx, __),
                         Arrays.asList(__, XX, xx, XX, xx, XX, xx),
                         Arrays.asList(__, xx, __, __, __, xx, __),
@@ -46,29 +33,64 @@ public class RandomGameSimulator {
                         Arrays.asList(__, xx, __, xx, __, __, __),
                         Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
         
-        Board board = Board.ofQuadrantNWBlocksWalled(
+        Board board2 = Board.ofQuadrantNWBlocksWalled(
                 Arrays.asList(Arrays.asList(__, __, __, __, __, __, __),
                               Arrays.asList(__, __, __, __, __, __, __),
                               Arrays.asList(__, XX, XX, XX, XX, XX, xx),
                               Arrays.asList(__, XX, __, __, __, __, __),
                               Arrays.asList(__, XX, __, __, __, __, __),
                               Arrays.asList(__, xx, __, __, __, __, __)));
+        
+        Board board3 = Board.ofQuadrantNWBlocksWalled(
+                Arrays.asList(
+                  Arrays.asList(__, __, __, __, __, xx, __),
+                  Arrays.asList(__, XX, xx, XX, xx, XX, xx),
+                  Arrays.asList(__, xx, __, __, __, xx, __),
+                  Arrays.asList(xx, XX, __, XX, XX, XX, XX),
+                  Arrays.asList(__, xx, __, xx, __, __, __),
+                  Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
 
         ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player(PlayerID.PLAYER_1, 30, new Cell(1, 1), 1, 3));
-        players.add(new Player(PlayerID.PLAYER_2, 30, new Cell(Cell.COLUMNS - 2, 1), 1, 3));
-        players.add(new Player(PlayerID.PLAYER_3, 30, new Cell(Cell.COLUMNS - 2, Cell.ROWS - 2), 1, 3));
-        players.add(new Player(PlayerID.PLAYER_4, 30, new Cell(1, Cell.ROWS - 2), 1, 3));
+        
+        players.add(new Player(PlayerID.PLAYER_1, 30000, new Cell(1, 1), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_2, 30000, new Cell(Cell.COLUMNS - 2, 1), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_3, 30000, new Cell(Cell.COLUMNS - 2, Cell.ROWS - 2), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_4, 30000, new Cell(1, Cell.ROWS - 2), 2, 3));
         
         
-        GameState GS = new GameState(board2, players);
+        GameState GS = new GameState(board3, players);
 
         while (!GS.isGameOver()) {
-            Set<PlayerID> randomBombDropEvents = randomEvent.randomBombDropEvents();
-            Map<PlayerID, Optional<Direction>> randomSpeedChangeEvents = randomEvent.randomSpeedChangeEvents();
             
             GameStatePrinter.printGameState(GS);
+
+            Map<PlayerID, Optional<Direction>> randomSpeedChangeEvents = randomEvent.randomSpeedChangeEvents();
+            Set<PlayerID> randomBombDropEvents = randomEvent.randomBombDropEvents();
+            
+            boolean debugActivated = false;
+            
+            if (debugActivated && (!randomBombDropEvents.isEmpty() || !randomSpeedChangeEvents.isEmpty())) {
+                System.out.println();
+                
+                for (PlayerID playerID : randomBombDropEvents) {
+                    System.out.println("Le joueur n°" + playerID.toString().charAt(7) + " va poser une bombe !");
+                }
+                
+                for (Map.Entry<PlayerID, Optional<Direction>> entry : randomSpeedChangeEvents.entrySet()) {
+                    if (entry.getValue().isPresent()) {
+                        System.out.println("Le joueur n°" + entry.getKey().toString().charAt(7) + " va dans la direction " + entry.getValue().get() + ".");
+                    } else {
+                        System.out.println("Le joueur n°" + entry.getKey().toString().charAt(7) + " va s'arrêter !");
+                    }
+                    
+                }
+
+                Thread.sleep(1000);
+            }
+            
             GS = GS.next(randomSpeedChangeEvents, randomBombDropEvents);
+            
+
             
             Thread.sleep(50);
         }
