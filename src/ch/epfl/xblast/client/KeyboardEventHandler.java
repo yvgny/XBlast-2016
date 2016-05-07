1400916dd40a46d42b57f3a1a9b3bf44aba2fb5e
Sacha Kozma <sacha.kozma@gmail.com>
2016-05-07 14:24:23 +0200

    New Keyboard Event Handler for human interaction

---
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -0,0 +1,54 @@
+package ch.epfl.xblast.client;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import ch.epfl.xblast.PlayerAction;
+
+/**
+ * Représente un auditeur d'événements clavier pour contrôler un joueur simulé
+ * 
+ * @author Sacha Kozma, 260391
+ * @author Alexia Bogaert, 258330
+ *
+ */
+public final class KeyboardEventHandler implements KeyListener {
+    private final Map<Integer, PlayerAction> actions;
+    private final Consumer<PlayerAction> actionConsumer;
+
+    /**
+     * Construit un auditeur d'évenements clavier
+     * 
+     * @param actions
+     *            Une table associant des actions de joueur à des codes de
+     *            touche
+     * @param actionConsumer
+     *            Un consommateur d'action de joueur
+     */
+    public KeyboardEventHandler(Map<Integer, PlayerAction> actions,
+            Consumer<PlayerAction> actionConsumer) {
+        this.actions = Collections.unmodifiableMap(new HashMap<>(actions));
+        this.actionConsumer = actionConsumer;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Unused
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (actions.containsKey(e.getKeyCode()))
+            actionConsumer.accept(actions.get(e.getKeyCode()));
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Unused
+    }
+
+}
